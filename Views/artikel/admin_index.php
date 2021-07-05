5a0bfa5b73819b75f4584281dd85afe9297dc6bb
irfanhilmi04 <56240221+irfanhilmi04@users.noreply.github.com>
2021-07-05 19:29:59 +0700

    Add files via upload

--- Views/artikel/admin_index.php
+++ Views/artikel/admin_index.php
@@ -1,5 +1,9 @@
 <?= $this->include('template/admin_header'); ?>
 
+<form method="get" class="form-search">
+    <input type="text" name="q" value="<?= $q; ?>" placeholder="Cari data">
+    <input type="submit" value="Cari" class="btn btn-primary">
+</form>
 <table class="table">
     <thead>
         <tr>
@@ -39,5 +43,6 @@
         </tr>
     </tfoot>
 </table>
+<?= $pager->only(['q'])->links(); ?>
 
 <?= $this->include('template/admin_footer'); ?>
